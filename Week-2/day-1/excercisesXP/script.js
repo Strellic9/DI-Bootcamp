2c61f51e07223c417cee02260d6a9d003dfd38f6
Aleksei Baranenko <aleks.bar239@gmail.com>
2022-06-26 14:55:42 +0300

    added w2d1 exc partially

---
+++ Week-2/day-1/excercisesXP/script.js
@@ -0,0 +1,38 @@
+
+// EXC 1 
+let FavFood =  'salmon';
+let FavMeal = "lunch";
+
+console.log('i love', FavFood, 'so much its unreal. i would eat', FavFood, 'at every', FavMeal, 'if i could.');
+
+// EXC 2 
+
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries.length
+let myWatchedSeriesSentence = 'Though I didnt watch any of these if Im honest. I didnt watch much of any series in my life, apart from first 4 seasons of Game of the Thrones.';
+console.log('one of the serials I watched was', myWatchedSeries[1], '. Ive only watched like', myWatchedSeriesLength, 'of these.', myWatchedSeriesSentence);
+
+myWatchedSeries[3]='friends';
+myWatchedSeries.push('Game of Thrones');
+myWatchedSeries.unshift('Hellsing');
+myWatchedSeries.splice(1,1)
+console.log(myWatchedSeries[1][2]);
+console.log(myWatchedSeries);
+
+
+
+
+
+// EXC 3 
+
+
+
+// EXC 4 
+
+
+
+// EXC 5 
+
+
+
+// EXC 6 
---
+++ Week-2/day-1/lesson/script.js
@@ -0,0 +1,28 @@
+let addressNumber =  21;
+let adressStreet = "Levontine";
+let country = "Israel";
+let space = " ";
+let globalAdress = addressNumber + space + adressStreet + space + country;
+
+console.log (globalAdress);
+
+let BY = 1998;
+let FY = 2023;
+let PA1 = 2023-1998-1;
+let PA2 = 2023-1998;
+console.log('i will be',PA1,'or',PA2,'in',FY);
+
+let arr = [];
+let arr1 = new Array();
+arr2=arr1;
+str1 = 'ayy lmao';
+ar0 = [1,2,3];
+ar0[3] = 5;
+
+console.log("arr1=>", arr);
+console.log("ar0=>", ar0);
+
+let anm = ['cat','dog','fish','rabbit','cow'];
+console.log(anm[1]);
+anm.push('horse');
+console.log(anm.length);
