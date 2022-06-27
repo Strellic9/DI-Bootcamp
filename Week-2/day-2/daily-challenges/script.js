a97bc0426ffbbb4ce0e170ed347871e0cdba246c
Aleksei Baranenko <aleks.bar239@gmail.com>
2022-06-27 15:25:40 +0300

    fixed w2d2 DaCh

--- Week-2/day-2/daily-challenges/script.js
+++ Week-2/day-2/daily-challenges/script.js
@@ -1,12 +1,17 @@
 
 console.log('_________EXC 1 DONE__________')
 
+// let sentence = 'Self-taught, and not half bad. Still, your technique lacks something. Ahh! You deny your weapon its purpose!'
 let sentence = 'Self-taught, and not half bad. Still, your technique lacks something. Ahh! You deny your weapon its purpose!'
 let wordNot = sentence.indexOf('not');
 let wordBad = sentence.indexOf('bad');
-if (wordNot<wordBad) {
+
+if (wordNot<0) {
+	console.log(sentence); 
+} else if (wordBad<0) {
+	console.log(sentence); 
+} else if (wordNot<wordBad) {
 	console.log(sentence.substring(0,wordNot-1), 'good', sentence.substring(wordBad+3,sentence.length));
 } else {
 	console.log(sentence);
 }
-	// console.log(sentence);
---
+++ Week-2/day-2/excerciseXPgold/script.js
@@ -0,0 +1,16 @@
+let ask=prompt ('What language do u speak?');
+ask1=ask.toLowerCase();
+console.log(ask1);
+if (ask1=="english") {
+	console.log("hello");
+} else if (ask1 == 'hebrew') {
+	console.log('shalom');
+} else if (ask1 == 'french') {
+	console.log ('bonjour')
+} else {
+	console.log('01110011 01101111 01110010 01110010 01111001');
+}
+
+
+console.log('_________EXC 1 DONE__________')
+
