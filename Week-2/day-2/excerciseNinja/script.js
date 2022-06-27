a32fc061c87f7fd8f8db67abc172db91999dcea0
Aleksei Baranenko <aleks.bar239@gmail.com>
2022-06-27 16:28:00 +0300

    fixed w2d2 gold, added part ninja

--- Week-2/day-2/excerciseNinja/script.js
+++ Week-2/day-2/excerciseNinja/script.js
@@ -1,15 +1,22 @@
-let ask=prompt ('What language do u speak?');
-ask1=ask.toLowerCase();
-// english
-if (ask1=="english") {
-	console.log("hello");
-} else if (ask1 == 'hebrew') {
-	console.log('shalom');
-} else if (ask1 == 'french') {
-	console.log ('bonjour')
-} else {
-	console.log('01110011 01101111 01110010 01110010 01111001');
-}
+// let x=1995;
+// let y=2001;
 
+// if (x<y) {x1=x; x2=y;}
+// else if (x>y) {x1=y; x2=x;}
+// else {console.log('Enter different years')}
+// // x1 is older
+// console.log(x2-x1+x2);
 
 console.log('_________EXC 1 DONE__________')
+
+let zip=prompt('Give me your ZIP code pls');
+
+// if (zip.search(' ')!=='-1') {
+// 	console.log('error');
+// } else if (zip.search([a-zA-Z])!=='-1') {
+// 	console.log('error');
+// } else if (zip.length!==5) {
+// 	console.log('error');
+// } else {console.log("success")}
+
+console.log(zip.search([a-zA-Z]));
