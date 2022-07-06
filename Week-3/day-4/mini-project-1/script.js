141d1593ae2d3a53d5aa73c311c8ea19cb6d3dba
Aleksei Baranenko <aleks.bar239@gmail.com>
2022-07-06 14:57:49 +0300

    added w3d4 project1

--- Week-3/day-4/mini-project-1/script.js
+++ Week-3/day-4/mini-project-1/script.js
@@ -1,49 +1,81 @@
+let color_col = 3;
+let color_row = 8;
+let color_count = color_col * color_row;
 
-// ACTIVATES ON BUTTON ON HTML PAGE
+let main_col = 60;
+let main_row = 50;
+let main_count = main_col * main_row;
 
-function bottlesLyrics () {
-	let bottlesNumber=parseInt(prompt('Throw me a number of bottles'));
-	for (let i=0; i<3; i++) {
-		// console.log(bottlesNumber);
-		if (isNaN(bottlesNumber)==true || (bottlesNumber<2)) {
-			bottlesNumber=parseInt(prompt('Again. Only number, above 1'));
-		} else break;
-	}
-	if (isNaN(bottlesNumber)==true || (bottlesNumber<2)) {
-		alert('u stoopid');
-		return;
-	}
-	
-	// ↑ SUDA PROMPT S PROVERKOY
-	console.log('we start a song with',bottlesNumber,'bottles');
-	console.log('');
+let sidebar = document.getElementById('sidebar');
+let main = document.querySelector('#main');
 
-	let bottlesCount=1;
-	console.log(' -> Take _'+bottlesCount+'_ down, pass it around');
-	bottlesNumber-=bottlesCount;
-	bottlesCount++;
-	console.log(' -> we have now '+bottlesNumber+' bottles');
-	console.log('');
+var color = '';
 
-	while (bottlesNumber>bottlesCount) {
-		console.log(' -> Take _'+bottlesCount+'_ down, pass them around');
-		bottlesNumber-=bottlesCount;
-		bottlesCount++;
-		console.log(' -> we have now '+bottlesNumber+' bottles');
-		console.log('');
-	}
+for (let i = 0; i < color_count; i++) {
+  let div = document.createElement('div');
+  div.style.backgroundColor = generateRandomColor();
 
-	// console.log(bottlesNumber,bottlesCount)
+  console.log(div.style.backgroundColor);
 
-	if (bottlesNumber==1) {
-		console.log(' -> Take _'+bottlesNumber+'_ down, pass it around');
-		console.log(' -> no bottle of beer on the wall');
-		console.log('');
-	} else {
-		console.log(' -> Take _'+bottlesNumber+'_ down, pass it around');
-		console.log(' -> no bottle of beer on the wall');
-		console.log('');
-	}
-
+  sidebar.appendChild(div);
+  div.addEventListener('click', function (e) {
+    color = div.style.backgroundColor;
+    console.log(div.style.backgroundColor);
+  })
 }
+
+// _____________________begin
+
+var mouse = false;
+function mousedown()
+  {
+    mouse = true;
+  }
+  function mouseup()
+  {
+    mouse =false;
+  }
+
+for (var i = 0; i < main_count; i++) {
+  let div = document.createElement('div');
+  div.className = 'boxInMain';
+  // div.addEventListener('mousedown', function(e) {
+  //   div.style.backgroundColor = color;
+
+  // }); 
+
+  div.addEventListener('mouseover', function (e) {
+    if(mouse)
+    {
+      div.style.backgroundColor = color;
+    }
+    else
+    return;
+  })
+
+  main.appendChild(div);
+}
+
+// _____________end
+
+function generateRandomColor(){
+  let letters = '0123456789ABCDEF'
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random()*16)]
+  }
+  return color;
+}
+
+function clearField () {
+  console.log('clear activated');
+  let boxes = document.getElementsByClassName('boxInMain');
+  // boxes[0].style.backgroundColor = 'white';
+  for (let i=0; i<boxes.length; i++) {
+    boxes[i].style.backgroundColor = 'white';
+  }
+}
+
+
+
 
