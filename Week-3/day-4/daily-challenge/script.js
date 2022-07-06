67d9a85655748e8fc9cd52a36a890f3eb4062f97
Aleksei Baranenko <aleks.bar239@gmail.com>
2022-07-06 14:57:20 +0300

    added w3d4 project1

--- Week-3/day-4/daily-challenge/script.js
+++ Week-3/day-4/daily-challenge/script.js
@@ -1,33 +1,34 @@
-// Instructions
-// Prompt the user for several words (separated by commas).
-// Put the words into an array.
-// Console.log the words one per line, in a rectangular frame as seen below.
-// Check out the Hints and Requirements below.
-// For example, if the user gives you:
-// Hello, World, in, a, frame
-// you will transform it to : ["Hello", "World", "in", "a", "frame"]
+let color_col=3;
+var color_row=8;
+let color_count=color_col*color_row;
+
+let main_col=60;
+let main_row=50;
+let main_count = main_row*main_col;
+
+let sidebar = document.getElementById('sidebar');
+let main = document.querySelector('#main');
 
 
-// let str=prompt('gib me words, separated by commas and whitespace');
-// INSERT: sus, sus, sus, sus, Amogus
-let str='sus1, sus2, sus3, sus4, Amogus';
-var arr=str.split(', ');
-console.log(arr);
-
-var max=0;
-
-for (let i of arr) {
-	if (i.length>max) max=i.length;
+for (let i=0; i<color_count; i++) {
+	let div = document.createElement('div');
+	div.style.backgroundColor = generateRandomColor ();
+	sidebar.appendChild(div);
 }
 
-var star='*';
-var ws=' ';
 
-for (let i=-1; i<arr.length+1; i++) {
-	if (i==-1 || i==(arr.length)) {
-		console.log(star.repeat(max+4));
-	} else {
-		let dl=arr[i].length;
-		console.log(star,arr[i],ws.repeat(max-dl)+star);
+for (let i=0; i<main_count; i++) {
+	let div = document.createElement('div');
+	// div.style.backgroundColor = generateRandomColor ();
+	main.appendChild(div);
+
+}
+
+function generateRandomColor () {
+	let letters='0123456789ABCDEF';
+	let color= '#';
+	for (let i=0; i<6; i++) {
+		color += letters[Math.floor(Math.random*16)]
 	}
+	return color;
 }
